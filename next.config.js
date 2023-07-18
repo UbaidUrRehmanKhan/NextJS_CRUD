5afd535eeff65ae6d77dc896c5577e5a0d853511
Ubaid Ur Rehman <Ubaid.Rehman@futurenow.com>
2023-07-19 00:05:21 +0500

    Updated Readme file

--- next.config.js
+++ next.config.js
@@ -1,8 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
-    // MONGODB_URI: "mongodb://127.0.0.0:27017/nextjs-crud",
-    MONGODB_URI: "mongodb+srv://urehman:523193@myprojectcluster.jdcpbjy.mongodb.net/users"
+    // MONGODB_URI: "mongodb+srv://urehman:<password>@myprojectcluster.jdcpbjy.mongodb.net/users"
   },
   reactStrictMode: true
 }
